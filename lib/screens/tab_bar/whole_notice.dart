a76c12b144fada16f70f4fba6343d83f174b07eb
logicallaw <logicallawbio@gmail.com>
2025-01-09 17:14:35 +0900

    feat: 카테고리 UI 구현

--- lib/screens/tab_bar/whole_notice.dart
+++ lib/screens/tab_bar/whole_notice.dart
@@ -51,165 +51,94 @@ class _WholeNoticePageState extends State<WholeNoticePage> {
       });
     }
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Color(0xEB292929),
-              ),
-              child: _isLoading
-                  ? const Center(child: CircularProgressIndicator())
-                  : _error.isNotEmpty
-                  ? Center(child: Text('Error: $_error'))
-                  : ListView(
-                children: [
-                  if (_notices['headline']!.isNotEmpty)
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _showHeadlines = !_showHeadlines;
-                        });
-                      },
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Text(
-                              '중요 공지사항',
-                              style: TextStyle(
-                                  fontFamily: 'Pretendard',
-                                  fontSize: 16,
-                                  color: Colors.white),
-                            ),
-                          ),
-                          if (_showHeadlines)
-                            ..._notices['headline']!.map((notice) {
-                              return Container(
-                                decoration: const BoxDecoration(
-                                  color: Color(0xFF222222), // 배경색
-                                  border: Border(
-                                    bottom: BorderSide(
-                                      color: Color(0xFF222222), // 하단 테두리 색상
-                                      width: 2.0,
-                                    ),
-                                  ),
-                                ),
-                                child: ListTile(
-                                  title: Text(
-                                    notice['title'] ?? 'No Title',
-                                    style: const TextStyle(
-                                        fontFamily: 'Pretendard',
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w500,
-                                        letterSpacing: 1.2,
-                                        color: Colors.white), // 제목 글자색
-                                  ),
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => WebPage(
-                                            url: notice['link'] ?? ''),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              );
-                            }).toList(),
-                        ],
+          Container(
+            color: const Color(0xFF222222),
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _showHeadlines = true;
+                    });
+                    print('중요 버튼 클릭됨');
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 2.0,
+                      ),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: const Text(
+                      '중요',
+                      style: TextStyle(
+                        fontFamily: 'Pretendard',
+                        fontSize: 13.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
                       ),
                     ),
-
-                  // 일반 공지사항(General Notices)
-                  if (_notices['general']!.isNotEmpty)
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _showHeadlines = false;
+                    });
+                    print('일반 버튼 클릭됨');
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                    decoration: BoxDecoration(
+                      color:  Colors.transparent,
+                      border: Border.all(color: Colors.blue, width: 2.0),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: const Text(
+                      '일반',
+                      style: TextStyle(
+                        fontFamily: 'Pretendard',
+                        fontSize: 13.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    print('새로고침 버튼 클릭됨');
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(4.0),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      border: Border.all(color: Colors.white, width: 2.0),
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: const Row(
                       children: [
-                        const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            '일반 공지사항',
-                            style: TextStyle(
-                                fontFamily: 'Pretendard',
-                                fontSize: 16,
-                                color: Colors.white),
-                          ),
-                        ),
-                        ..._notices['general']!.map((notice) {
-                          return Container(
-                            decoration: const BoxDecoration(
-                              color: Color(0xFF292929), // 배경색
-                              border: Border(
-                                bottom: BorderSide(
-                                  color: Color(0x8C525050), // 하단 테두리 색상
-                                  width: 1.0,
-                                ),
-                              ),
-                            ),
-                            child: ListTile(
-                              title: Text(
-                                notice['title'] ?? 'No Title',
-                                style: const TextStyle(
-                                    fontFamily: 'Pretendard',
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.normal,
-                                    color: Colors.white), // 제목 글자색
-                              ),
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => WebPage(
-                                        url: notice['link'] ?? ''),
-                                  ),
-                                );
-                              },
-                            ),
-                          );
-                        }).toList(),
+                        Icon(Icons.refresh, color: Colors.white, size: 16.0),
                       ],
                     ),
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
           ),
-          if (_notices['pages']!.isNotEmpty)
-            Container(
-              color: const Color(0xFF292929), // 하단 배경색
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: _initialPages.map<Widget>((pageData) {
-                    final int pageNumber = pageData['page']; // 페이지 번호 추출
-                    final bool isCurrentPage = (pageNumber == _currentPage);
-                    return TextButton(
-                      onPressed: isCurrentPage
-                          ? null
-                          : () {
-                        _loadNotices(pageNumber); // 해당 페이지로 이동
-                      },
-                      child: Text(
-                        pageNumber.toString(),
-                        style: TextStyle(
-                          color: isCurrentPage ? Colors.white : Colors.white60,
-                          fontWeight: isCurrentPage
-                              ? FontWeight.bold
-                              : FontWeight.normal,
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                ),
-              ),
-            ),
         ],
       ),
     );
