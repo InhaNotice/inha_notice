acea7a81a583790b7f402d46e66a5fa690656268
logicallaw <logicallawbio@gmail.com>
2025-01-22 15:26:00 +0900

    style: 앱 아이콘 업데이트 및 온보딩 업데이트

--- lib/screens/onboarding/onboarding_screen.dart
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -1,23 +1,87 @@
 import 'package:flutter/material.dart';
 
 import 'package:inha_notice/screens/bottom_navigation/bottom_nav_bar_page.dart';
+import 'package:inha_notice/fonts/font.dart';
+import 'package:inha_notice/themes/theme.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 1), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const BottomNavBarPage()),
-      );
-    });
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
 
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 1), () {
+      if (mounted) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const BottomNavBarPage()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Center(
-        child: Image.asset(
-          'assets/images/onboarding.png',
-          fit: BoxFit.cover,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text.rich(
+              TextSpan(
+                children: [
+                  const TextSpan(
+                    text: '인',
+                    style: TextStyle(
+                      fontFamily: Font.kDefaultFont,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: '하',
+                    style: TextStyle(
+                      fontFamily: Font.kDefaultFont,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).textTheme.bodyMedium?.color ?? Theme.of(context).defaultColor
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text.rich(
+              TextSpan(
+                children: [
+                  const TextSpan(
+                    text: '공',
+                    style: TextStyle(
+                      fontFamily: Font.kDefaultFont,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  TextSpan(
+                    text: '지',
+                    style: TextStyle(
+                      fontFamily: Font.kDefaultFont,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold,
+                        color: Theme.of(context).textTheme.bodyMedium?.color ?? Theme.of(context).defaultColor
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
