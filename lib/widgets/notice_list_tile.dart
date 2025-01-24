e23ffcc4f3a59b7214436a647be9579361eea82f
logicallaw <logicallawbio@gmail.com>
2025-01-24 16:44:35 +0900

    fix: 삭제된 WebPage load 코드 추가

--- lib/widgets/notice_list_tile.dart
+++ lib/widgets/notice_list_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:inha_notice/themes/theme.dart';
 import 'package:inha_notice/fonts/font.dart';
+import 'package:inha_notice/screens/web_page.dart';
 
 class NoticeListTile extends StatefulWidget {
   final Map<String, dynamic> notice;
@@ -37,80 +38,102 @@ class _NoticeListTileState extends State<NoticeListTile> {
     final textColor = widget.isRead
         ? readTextColor
         : Theme.of(context).textTheme.bodyMedium?.color ??
-        Theme.of(context).defaultColor;
+            Theme.of(context).defaultColor;
     // 안전한 데이터 접근
-    final title = widget.notice.containsKey('title') ? widget.notice['title'] ?? '제목이 없는 게시글입니다' : '제목이 없는 게시글입니다';
-    final date = widget.notice.containsKey('date') ? widget.notice['date'] ?? '' : '';
-    final access = widget.notice.containsKey('access') ? widget.notice['access'] : null;
+    final title = widget.notice.containsKey('title')
+        ? widget.notice['title'] ?? '제목이 없는 게시글입니다'
+        : '제목이 없는 게시글입니다';
+    final date =
+        widget.notice.containsKey('date') ? widget.notice['date'] ?? '' : '';
+    final access =
+        widget.notice.containsKey('access') ? widget.notice['access'] : null;
     // final writer = widget.notice.containsKey('writer') ? widget.notice['writer'] ?? '' : '';
 
-    return Column(children: [
-      Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20.0),
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(
-              color: Theme.of(context).noticeBorderColor,
-              width: 1.0,
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: Theme.of(context).noticeBorderColor,
+                width: 1.0,
+              ),
+              top: BorderSide.none,
+              left: BorderSide.none,
+              right: BorderSide.none,
             ),
-            top: BorderSide.none,
-            left: BorderSide.none,
-            right: BorderSide.none,
           ),
         ),
-      ),
-      ListTile(
-          title: Text(
-            title,
-            style: TextStyle(
-              fontFamily: Font.kDefaultFont,
-              fontSize: 16.0,
-              fontWeight: FontWeight.normal,
-              color: textColor,
+        ListTile(
+            title: Text(
+              title,
+              style: TextStyle(
+                fontFamily: Font.kDefaultFont,
+                fontSize: 16.0,
+                fontWeight: FontWeight.normal,
+                color: textColor,
+              ),
             ),
-          ),
-          subtitle: Row(
-            children: [
-              Expanded(
-                child: Row(
-                  children: [
-                    if (widget.notice['date'] != null)
-                      Text(
-                        date,
-                        style: TextStyle(
-                          fontFamily: Font.kDefaultFont,
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.normal,
-                          color: textColor.withOpacity(0.6),
+            subtitle: Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      if (widget.notice['date'] != null)
+                        Text(
+                          date,
+                          style: TextStyle(
+                            fontFamily: Font.kDefaultFont,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.normal,
+                            color: textColor.withOpacity(0.6),
+                          ),
                         ),
-                      ),
-                    SizedBox(width: Theme.of(context).visualDensity.horizontal),
-                    if (widget.notice['access'] != null)
-                      Text(
-                        '조회 $access',
-                        style: TextStyle(
-                          fontFamily: Font.kDefaultFont,
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.normal,
-                          color: textColor.withOpacity(0.6),
+                      SizedBox(
+                          width: Theme.of(context).visualDensity.horizontal),
+                      if (widget.notice['access'] != null)
+                        Text(
+                          '조회 $access',
+                          style: TextStyle(
+                            fontFamily: Font.kDefaultFont,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.normal,
+                            color: textColor.withOpacity(0.6),
+                          ),
                         ),
-                      ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              IconButton(
-                icon: Icon(
-                  widget.isBookmarked ? Icons.bookmark : Icons.bookmark_border,
-                  color: widget.isBookmarked ? Colors.yellow : Colors.grey,
+                IconButton(
+                  icon: Icon(
+                    widget.isBookmarked
+                        ? Icons.bookmark
+                        : Icons.bookmark_border,
+                    color: widget.isBookmarked ? Colors.yellow : Colors.grey,
+                  ),
+                  onPressed: () async {
+                    if (widget.notice.containsKey('id') &&
+                        widget.notice['id'] != null) {
+                      await widget
+                          .toggleBookmark(widget.notice['id'].toString());
+                    }
+                  },
                 ),
-                onPressed: () async {
-                  if (widget.notice.containsKey('id') && widget.notice['id'] != null) {
-                    await widget.toggleBookmark(widget.notice['id'].toString());
-                  }
-                },
-              ),
-            ],
-          ))
-    ]);
+              ],
+            ),
+            onTap: () async {
+              await widget.markAsRead(widget.notice['id'].toString());
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => WebPage(
+                    url: widget.notice['link'] ?? Font.kEmptyString,
+                  ),
+                ),
+              );
+            }),
+      ],
+    );
   }
 }
