542b459dcb24e640578bd4a8db067367f0368260
logicallaw <logicallawbio@gmail.com>
2025-01-28 14:40:14 +0900

    feat: 학과 저장 로직을 변경하였습니다.
    - 학과 저장시 기존 국문에서 영문으로 변경하였습니다.

--- lib/screens/bottom_navigation/more/major_setting_page.dart
+++ lib/screens/bottom_navigation/more/major_setting_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:inha_notice/utils/major_storage.dart';
 import 'package:inha_notice/fonts/font.dart';
 import 'package:inha_notice/themes/theme.dart';
+import 'package:inha_notice/screens/bottom_navigation/more/major_groups.dart';
 
 class MajorSettingPage extends StatefulWidget {
   const MajorSettingPage({super.key});
@@ -11,110 +12,16 @@ class MajorSettingPage extends StatefulWidget {
 }
 
 class _MajorSettingPageState extends State<MajorSettingPage> {
-  final TextEditingController _searchController = TextEditingController();
-  String? _currentMajor; // 현재 학과
-
-  final Map<String, List<String>> _majorGroups = {
-    '공과대학': [
-      '기계공학과',
-      '항공우주공학과',
-      '조선해양공학과',
-      '산업경영공학과',
-      '화학공학과',
-      '생명공학과',
-      '고분자공학과'
-      '신소재공학과',
-      '사회인프라공학과',
-      '환경공학과',
-      '공간정보공학과',
-      '건축학부',
-      '에너지자원공학과',
-      '전기공학과',
-      '전자공학과',
-      '정보통신공학과'
-    ],
-    '자연과학대학': [
-      '수학과',
-      '통계학과',
-      '물리학과',
-      '화학과',
-      '생명과학과',
-      '해양학과',
-    ],
-    '경영대학': [
-      '식품영양학과',
-      '경영학과',
-      '글로벌금융학과',
-      '아태물류학과',
-      '국제통상학과'
-    ],
-    '사범대학': [
-      '국어교육과',
-      '영어교육과',
-      '사회교육과',
-      '체육교육과',
-      '교육학과',
-      '수학교육과',
-    ],
-    '사회과학대학': [
-      '행정학과',
-      '정치외교학과',
-      '미디어커뮤니케이션학과',
-      '경제학과',
-      '소비자학과',
-      '아동심리학과',
-      '사회복지학과',
-    ],
-    '문과대학': [
-      '한국어문학과',
-      '사학과',
-      '철학과',
-      '중국학과',
-      '일본언어문화학과',
-      '영어영문학과',
-      '프랑스어문화학과',
-      '문화콘텐츠문화경영학과',
-    ],
-    '의과대학': [
-      '의예과',
-      '간호학과'
-    ],
-    '예술체육대학': [
-      '조형예술학과',
-      '디자인학부',
-      '스포츠과학과',
-      '연극영화학과',
-      '의류디자인학과',
-    ],
-    '국제학부': [
-      'IBT학과',
-      'ISE학과'
-    ],
-    '미래융합대학': [
-      '메카트로닉스공학과',
-      '소프트웨어융합공학과',
-      '산업경영학과',
-      '금융투자학과'
-    ],
-    '소프트웨어융합대학': [
-      '인공지능공학과',
-      '스마트모빌리티공학과',
-      '데이터사이언스학과',
-      '디자인테크놀리지학과',
-      '컴퓨터공학과'
-    ],
-    '프런티어학부대학': [
-      '자유전공학부'
-    ]
-  };
-
-  Map<String, List<String>> _filteredMajorGroups = {};
+  late TextEditingController _searchController;
+  late String? _currentMajor;
+  late Map<String, Map<String, String>> _filteredMajorGroups;
+  late List<String> _filteredMajors;
 
   @override
   void initState() {
     super.initState();
     _initializeStorage();
-    _filteredMajorGroups = _majorGroups;
+    _setupFields();
   }
 
   Future<void> _initializeStorage() async {
@@ -122,60 +29,93 @@ class _MajorSettingPageState extends State<MajorSettingPage> {
     await _loadCurrentMajor();
   }
 
+  void _setupFields() {
+    _searchController = TextEditingController();
+    _filteredMajorGroups = majorGroups;
+    _filteredMajors = [];
+    _currentMajor = '';
+  }
+
   Future<void> _loadCurrentMajor() async {
-    String? major = await MajorStorage.getMajor();
+    String? majorKey = await MajorStorage.getMajor();
     setState(() {
-      _currentMajor = major;
+      _currentMajor = _translateToKorean(majorKey);
     });
+  }
+
+  // 저장된 영문 학과명을 국문 학과명으로 번역합니다.
+  String? _translateToKorean(String? majorKey) {
+    if (majorKey == null) return null;
+    for (var group in majorGroups.values) {
+      for (var entry in group.entries) {
+        if (entry.value == majorKey) {
+          return entry.key;
+        }
+      }
+    }
+    return null;
   }
 
   void _filterMajors(String query) {
     setState(() {
       if (query.isEmpty) {
-        _filteredMajorGroups = _majorGroups;
+        // 검색어가 없으면 단과대학 그룹으로 표시
+        _filteredMajorGroups = majorGroups;
+        _filteredMajors = [];
       } else {
-        _filteredMajorGroups = {
-          for (var entry in _majorGroups.entries)
-            if (entry.value.any((major) => major.contains(query)))
-              entry.key: entry.value
-                  .where((major) => major.contains(query))
-                  .toList(),
-        };
+        // 검색어가 있으면 학과 리스트만 표시
+        _filteredMajorGroups = {};
+        _filteredMajors = [
+          for (var group in majorGroups.values)
+            for (var major in group.keys)
+              if (major.contains(query)) major,
+        ];
       }
     });
   }
 
   Future<void> _saveMajor(String major) async {
-    await MajorStorage.saveMajor(major);
+    String majorKey = _translateToEnglish(major);
+    print(majorKey);
+    await MajorStorage.saveMajor(majorKey);
     setState(() {
       _currentMajor = major;
     });
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(
-          '$major 학과가 저장되었습니다.',
-          style: TextStyle(
-            fontFamily: Font.kDefaultFont,
-            fontSize: 16,
-            fontWeight: FontWeight.normal,
-            color: Theme.of(context).textTheme.bodyMedium?.color ?? Theme.of(context).defaultColor
-          )
-      )
-        , backgroundColor: Theme.of(context).scaffoldBackgroundColor,),
+      SnackBar(
+        content: Text('$major 학과가 저장되었습니다.',
+            style: TextStyle(
+                fontFamily: Font.kDefaultFont,
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+                color: Theme.of(context).textTheme.bodyMedium?.color ??
+                    Theme.of(context).defaultColor)),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
     );
+  }
+
+  // 국문 학과명을 영문 학과명으로 저장합니다.
+  String _translateToEnglish(String major) {
+    for (var group in majorGroups.values) {
+      if (group.containsKey(major)) {
+        return group[major]!;
+      }
+    }
+    throw Exception('학과를 찾을 수 없습니다.');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-            '학과 설정',
+        title: Text('학과 설정',
             style: TextStyle(
                 fontFamily: Font.kDefaultFont,
                 fontSize: 16,
                 fontWeight: FontWeight.normal,
-                color: Theme.of(context).textTheme.bodyMedium?.color ?? Theme.of(context).defaultColor
-            )),
+                color: Theme.of(context).textTheme.bodyMedium?.color ??
+                    Theme.of(context).defaultColor)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -190,12 +130,12 @@ class _MajorSettingPageState extends State<MajorSettingPage> {
                       fontFamily: Font.kDefaultFont,
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
-                      color: Theme.of(context).textTheme.bodyMedium?.color ?? Theme.of(context).defaultColor
-                  ),
+                      color: Theme.of(context).textTheme.bodyMedium?.color ??
+                          Theme.of(context).defaultColor),
                 ),
               )
             else
-               Padding(
+              Padding(
                 padding: const EdgeInsets.only(bottom: 16.0),
                 child: Text(
                   '학과를 설정해주세요!',
@@ -203,8 +143,8 @@ class _MajorSettingPageState extends State<MajorSettingPage> {
                       fontFamily: Font.kDefaultFont,
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
-                      color: Theme.of(context).textTheme.bodyMedium?.color ?? Theme.of(context).defaultColor
-                  ),
+                      color: Theme.of(context).textTheme.bodyMedium?.color ??
+                          Theme.of(context).defaultColor),
                 ),
               ),
             TextField(
@@ -217,11 +157,34 @@ class _MajorSettingPageState extends State<MajorSettingPage> {
             ),
             const SizedBox(height: 16),
             Expanded(
-              child: ListView(
+              child: _filteredMajors.isNotEmpty
+                  ? ListView.builder(
+                itemCount: _filteredMajors.length,
+                itemBuilder: (context, index) {
+                  final major = _filteredMajors[index];
+                  return ListTile(
+                    title: Text(major),
+                    onTap: () => _saveMajor(major),
+                  );
+                },
+              )
+                  : ListView(
                 children: _filteredMajorGroups.entries.map((entry) {
                   return ExpansionTile(
-                    title: Text(entry.key),
-                    children: entry.value
+                    title: Text(
+                      entry.key,
+                      style: TextStyle(
+                        fontFamily: Font.kDefaultFont,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.color ??
+                            Theme.of(context).defaultColor,
+                      ),
+                    ),
+                    children: entry.value.keys
                         .map(
                           (major) => ListTile(
                         title: Text(major),
