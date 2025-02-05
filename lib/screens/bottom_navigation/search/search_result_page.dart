96c3545fddfc7fe3a828f5a40801adbf1f5310fe
logicallaw <logicallawbio@gmail.com>
2025-02-05 16:00:10 +0900

    feat: update SearchScraper
    - new feature of SearchScraper: add id, body, date informations.

--- lib/screens/bottom_navigation/search/search_result_page.dart
+++ lib/screens/bottom_navigation/search/search_result_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'package:inha_notice/services/search_api.dart';
-import 'package:inha_notice/screens/web_page.dart';
 import 'package:inha_notice/fonts/font.dart';
+import 'package:inha_notice/screens/web_page.dart';
+import 'package:inha_notice/services/scraper/search_scraper.dart';
 
 typedef NoticesMap = Map<String, dynamic>;
 typedef PagesList = List<Map<String, dynamic>>;
@@ -17,7 +16,7 @@ class SearchResultPage extends StatefulWidget {
 }
 
 class _SearchResultPageState extends State<SearchResultPage> {
-  final SearchAPI _searchAPI = SearchAPI();
+  final SearchScraper _searchAPI = SearchScraper();
   final TextEditingController _searchController = TextEditingController();
   NoticesMap _notices = {'notices': [], 'pages': []};
   PagesList _initialPages = [];
@@ -76,95 +75,94 @@ class _SearchResultPageState extends State<SearchResultPage> {
         color: const Color(0xFF292929), // 로딩 상태에서도 배경색 유지
         child: _isLoading
             ? const Center(
-          child: CircularProgressIndicator(
-            color: Colors.white, // 로딩 인디케이터 색상
-          ),
-        )
+                child: CircularProgressIndicator(
+                  color: Colors.white, // 로딩 인디케이터 색상
+                ),
+              )
             : _error.isNotEmpty
-            ? Center(child: Text(
-            'Error: $_error', style: const TextStyle(color: Colors.white)))
-            : Column(
-          children: [
-            Expanded(
-              child: Container(
-                color: const Color(0xFF292929),
-                child: ListView(
-                  children: (_notices['notices'] ?? []).map<Widget>((notice) {
-                    return Container(
-                      decoration: const BoxDecoration(
-                        color: Color(0xFF222222),
-                        border: Border(
-                          bottom: BorderSide(
-                            color: Color(0xFF444444),
-                            width: 1.0,
+                ? Center(
+                    child: Text('Error: $_error',
+                        style: const TextStyle(color: Colors.white)))
+                : Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          color: const Color(0xFF292929),
+                          child: ListView(
+                            children: (_notices['notices'] ?? [])
+                                .map<Widget>((notice) {
+                              return Container(
+                                decoration: const BoxDecoration(
+                                  color: Color(0xFF222222),
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: Color(0xFF444444),
+                                      width: 1.0,
+                                    ),
+                                  ),
+                                ),
+                                child: ListTile(
+                                  title: Text(
+                                    notice['title'] ?? 'No Title',
+                                    style: const TextStyle(
+                                      fontFamily: Font.kDefaultFont,
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            WebPage(url: notice['link'] ?? ''),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              );
+                            }).toList(),
                           ),
                         ),
                       ),
-                      child: ListTile(
-                        title: Text(
-                          notice['title'] ?? 'No Title',
-                          style: const TextStyle(
-                            fontFamily: Font.kDefaultFont,
-                            fontWeight: FontWeight.normal,
-                            fontSize: 16,
-                            color: Colors.white,
-                          ),
-                        ),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  WebPage(url: notice['link'] ?? ''),
-                            ),
-                          );
-                        },
-                      ),
-                    );
-                  }).toList(),
-                ),
-              ),
-            ),
-            if (_initialPages.isNotEmpty)
-              Container(
-                color: const Color(0xFF292929),
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: _initialPages.map<Widget>((pageData) {
-                      final String pageNumber =
-                      pageData['page'].toString();
-                      final int startCount =
-                      int.parse(pageData['startCount']);
-                      final bool isCurrentPage =
-                          (startCount ~/ 10) + 1 == _currentPage;
+                      if (_initialPages.isNotEmpty)
+                        Container(
+                          color: const Color(0xFF292929),
+                          padding: const EdgeInsets.symmetric(vertical: 8.0),
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: _initialPages.map<Widget>((pageData) {
+                                final int pageNumber = pageData['page'];
+                                final int startCount = pageData['startCount'];
+                                final bool isCurrentPage =
+                                    (startCount ~/ 10) + 1 == _currentPage;
 
-                      return TextButton(
-                        onPressed: isCurrentPage
-                            ? null
-                            : () =>
-                            loadNotices(
-                                startCount: startCount),
-                        child: Text(
-                          pageNumber,
-                          style: TextStyle(
-                            color: isCurrentPage
-                                ? Colors.white
-                                : Colors.white60,
-                            fontWeight: isCurrentPage
-                                ? FontWeight.bold
-                                : FontWeight.normal,
+                                return TextButton(
+                                  onPressed: isCurrentPage
+                                      ? null
+                                      : () =>
+                                          loadNotices(startCount: startCount),
+                                  child: Text(
+                                    pageNumber.toString(),
+                                    style: TextStyle(
+                                      color: isCurrentPage
+                                          ? Colors.white
+                                          : Colors.white60,
+                                      fontWeight: isCurrentPage
+                                          ? FontWeight.bold
+                                          : FontWeight.normal,
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                            ),
                           ),
                         ),
-                      );
-                    }).toList(),
+                    ],
                   ),
-                ),
-              ),
-          ],
-        ),
       ),
     );
   }
---
+++ lib/services/scraper/search_scraper.dart
@@ -0,0 +1,113 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:html/parser.dart' as html_parser;
+import 'package:http/http.dart' as http;
+import 'package:inha_notice/constants/identifier_constants.dart';
+import 'package:inha_notice/constants/status_code_constants.dart';
+import 'package:inha_notice/selectors/search_tag_selectors.dart';
+
+class SearchScraper {
+  late String baseUrl;
+  late String collectionType;
+
+  SearchScraper() {
+    baseUrl = dotenv.get('SEARCH_URL');
+    collectionType = dotenv.get('COLLECTION');
+  }
+
+  Future<Map<String, dynamic>> fetchNotices(
+      String query, int startCount) async {
+    try {
+      // 크롤링 진행
+      final String connectUrl =
+          '$baseUrl?query=$query&collection=$collectionType&startCount=$startCount';
+      final response = await http.get(Uri.parse(connectUrl));
+
+      if (response.statusCode == StatusCodeSettings.kStatusOkay) {
+        final document = html_parser.parse(response.body);
+
+        // 검색된 공지사항 가져오기
+        final searchedNotices = fetchSearchedNotices(document);
+
+        // 페이지 번호 가져오기
+        final pages = fetchPages(document);
+
+        return {
+          'notices': searchedNotices,
+          'pages': pages,
+        };
+      } else {
+        throw Exception('Failed to load board page: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching notices: $e');
+    }
+  }
+
+  List<Map<String, dynamic>> fetchSearchedNotices(document) {
+    final notices = document.querySelectorAll(NoticeTagSelectors.kNoticeBoard);
+    final List<Map<String, String>> results = [];
+
+    for (var notice in notices) {
+      final titleTag = notice.querySelector(NoticeTagSelectors.kNoticeTitle);
+      final bodyTag = notice.querySelector(NoticeTagSelectors.kNoticeBody);
+      final dateTag = notice.querySelector(NoticeTagSelectors.kNoticeDate);
+
+      if (titleTag == null || bodyTag == null || dateTag == null) {
+        continue;
+      }
+
+      final postUrl = titleTag.attributes[NoticeTagSelectors.kNoticeTitleHref];
+
+      final id = makeUniqueNoticeId(postUrl);
+      final title = titleTag.text?.trim() ?? '';
+      final body = bodyTag.text?.trim() ?? '';
+      final link = postUrl;
+      final date = dateTag.text.trim();
+
+      results.add(
+          {'id': id, 'title': title, 'body': body, 'link': link, 'date': date});
+    }
+    return results;
+  }
+
+  List<Map<String, dynamic>> fetchPages(document) {
+    final List<Map<String, dynamic>> results = [];
+    final pages = document.querySelectorAll(PageTagSelectors.kPageBoard);
+    if (pages.isEmpty) return results;
+
+    final String? lastPageOnClick = pages.last.attributes['onclick'];
+    if (lastPageOnClick == null) return results;
+
+    final match = RegExp(r"doPaging\('(\d+)'\)").firstMatch(lastPageOnClick);
+    int lastPage = int.parse(match?.group(1) ?? '1');
+    if (lastPage != 1) {
+      lastPage = lastPage ~/ 10 + 1;
+    }
+    for (int i = 1; i <= lastPage; i++) {
+      final int page = i;
+      final int startCount = (i - 1) * 10;
+      final bool isCurrent = (i == 1) ? true : false;
+      results.add(
+          {'page': page, 'startCount': startCount, 'isCurrent': isCurrent});
+    }
+    return results;
+  }
+
+  String makeUniqueNoticeId(String postUrl) {
+    // postUrl이 빈 문자열인지 확인합니다.
+    if (postUrl.isEmpty) {
+      return IdentifierConstants.kUnknownId;
+    }
+
+    final List<String> postUrlList = postUrl.split('/');
+    // postUrlList가 정해진 규격을 따르는지 확인합니다.
+    if (postUrlList.length <= 6) {
+      return IdentifierConstants.kUnknownId;
+    }
+
+    final String provider = postUrlList[4];
+    final String postId = postUrlList[6];
+    final String uniqueNoticeId = '$provider-$postId';
+    return uniqueNoticeId;
+  }
+}
