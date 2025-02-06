28b536259c3dcd97ef4482728edd4042cd44676f
logicallaw <logicallawbio@gmail.com>
2025-02-06 17:18:23 +0900

    feat: 토픽 구독 방식 변경(NotificationSettingPage에서 관리 하도록 변경)
    - add FirebaseService: _subscribedTopics, subscribeToTopic(), unsubscribeFromTopic(), _isSubscribedToTopic(), _addSubscribedTopic(), _removeSubscribedTopic(), updateMajorSubscription() 추가
    - update MajorSettingPage: isMajorNotificationOn이 true인 경우에만 푸시알림 설정
    - update MajorUtils: 기존 푸시알림 함수를 FirebaseService로 이동
    - update NotificationSettingPage: 토픽 구독 방식 변경
    - update SharedPrefsManager: setMajorKey(), getPreviousMajorKey(), setSubscribedTopics(), getSubscribedTopics(), setIsSubscribedToAllUsers(), getIsSubscribedToAllUsers() 추가

--- lib/utils/shared_prefs_manager.dart
+++ lib/utils/shared_prefs_manager.dart
@@ -1,13 +1,19 @@
+import 'package:logger/logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPrefsManager {
   static final SharedPrefsManager _instance = SharedPrefsManager._internal();
+  static final logger = Logger();
+
   SharedPreferences? _prefs;
 
   // 캐싱을 위한 변수
+  static String? _cachedPreviousMajorKey;
   static String? _cachedMajorKey;
   static bool? _cachedAcademicNotification;
   static bool? _cachedMajorNotification;
+  static Set<String>? _cachedSubscribedTopics;
+  static bool? _cachedIsSubscribedToAllUsers;
 
   factory SharedPrefsManager() => _instance;
 
@@ -16,19 +22,29 @@ class SharedPrefsManager {
   /// SharedPreferences 초기화
   Future<void> initialize() async {
     _prefs = await SharedPreferences.getInstance();
-    // 초기화 시 캐싱 값 로드
-    _cachedMajorKey = _prefs?.getString('major_key');
+    _cachedMajorKey = _prefs?.getString('major-key');
+    _cachedPreviousMajorKey = _prefs?.getString('previous-major-key');
     _cachedAcademicNotification =
-        _prefs?.getBool('academic_notification') ?? false;
-    _cachedMajorNotification = _prefs?.getBool('major_notification') ?? false;
+        _prefs?.getBool('academic-notification') ?? false;
+    _cachedMajorNotification = _prefs?.getBool('major-notification') ?? false;
+    _cachedSubscribedTopics =
+        _prefs?.getStringList('subscribed_topics')?.toSet() ?? {};
+    _cachedIsSubscribedToAllUsers =
+        _prefs?.getBool('isSubscribedToAllUsers') ?? false;
   }
 
   SharedPreferences? get prefs => _prefs;
 
   /// 학과 키 저장 (캐싱 추가)
-  Future<void> setMajorKey(String majorKey) async {
-    _cachedMajorKey = majorKey; // 캐싱
-    await _prefs?.setString('major_key', majorKey);
+  Future<void> setMajorKey(String? currentMajorKey, String newMajorKey) async {
+    if (currentMajorKey != null) {
+      await _prefs?.setString('previous-major-key', currentMajorKey);
+      _cachedPreviousMajorKey = currentMajorKey;
+    }
+    _cachedMajorKey = newMajorKey;
+    await _prefs?.setString('major-key', newMajorKey);
+    logger.d(
+        "🔔 Successfully updated from previous major key: '$currentMajorKey' to new major key: '$newMajorKey'");
   }
 
   /// 학과 키 가져오기 (캐싱 활용)
@@ -36,10 +52,15 @@ class SharedPrefsManager {
     return _cachedMajorKey;
   }
 
+  /// 이전 학과 키 가져오기 (캐싱 활용)
+  String? getPreviousMajorKey() {
+    return _cachedPreviousMajorKey;
+  }
+
   /// 학사 알림 설정 키 저장 (캐싱 추가)
   Future<void> setAcademicNotificationOn(bool academicNotificationOn) async {
     _cachedAcademicNotification = academicNotificationOn; // 캐싱
-    await _prefs?.setBool('academic_notification', academicNotificationOn);
+    await _prefs?.setBool('academic-notification', academicNotificationOn);
   }
 
   /// 학사 알림 설정 키 가져오기 (캐싱 활용)
@@ -50,11 +71,35 @@ class SharedPrefsManager {
   /// 학과 알림 설정 키 저장 (캐싱 추가)
   Future<void> setMajorNotificationOn(bool majorNotificationOn) async {
     _cachedMajorNotification = majorNotificationOn; // 캐싱
-    await _prefs?.setBool('major_notification', majorNotificationOn);
+    await _prefs?.setBool('major-notification', majorNotificationOn);
   }
 
   /// 학과 알림 설정 키 가져오기 (캐싱 활용)
   bool getMajorNotificationOn() {
     return _cachedMajorNotification ?? false;
+  }
+
+  /// 구독된 토픽 목록 설정 (캐싱 추가)
+  Future<void> setSubscribedTopics(Set<String> subscribedTopics) async {
+    _cachedSubscribedTopics = subscribedTopics;
+    await _prefs?.setStringList('subscribed_topics', subscribedTopics.toList());
+  }
+
+  /// 구독된 토픽 목록 가져오기 (캐싱 활용)
+  Set<String> getSubscribedTopics() {
+    _cachedSubscribedTopics ??=
+        _prefs?.getStringList('subscribed_topics')?.toSet() ?? {};
+    return _cachedSubscribedTopics!;
+  }
+
+  /// 'all-users' 토픽 구독 여부 설정
+  Future<void> setIsSubscribedToAllUsers(bool isSubscribed) async {
+    _cachedIsSubscribedToAllUsers = isSubscribed;
+    await _prefs?.setBool('isSubscribedToAllUsers', isSubscribed);
+  }
+
+  /// 'all-users' 토픽 구독 여부 확인
+  bool getIsSubscribedToAllUsers() {
+    return _cachedIsSubscribedToAllUsers ?? false;
   }
 }
