68ac477cd5754704c2c4e08b8662f319072bd3e6
logicallaw <logicallawbio@gmail.com>
2025-02-10 17:26:45 +0900

    chore(NoticeListTile): 코드 리팩터링

--- lib/widgets/notice_list_tile.dart
+++ lib/widgets/notice_list_tile.dart
@@ -59,122 +59,128 @@ class _NoticeListTileState extends State<NoticeListTile> {
         ? Theme.of(context).readTextColor
         : Theme.of(context).textTheme.bodyMedium?.color ??
             Theme.of(context).defaultColor;
-    // 공지사항 제목
-    final title = widget.notice.containsKey('title')
+    final String title = widget.notice.containsKey('title')
         ? widget.notice['title'] ?? '제목이 없는 게시글입니다'
         : '제목이 없는 게시글입니다';
-    // 공지사항 날짜
-    final date =
-        widget.notice.containsKey('date') ? widget.notice['date'] ?? '' : '';
-    // 공지사항 조회수
-    final access =
-        widget.notice.containsKey('access') ? widget.notice['access'] : null;
+    final String body = widget.notice.containsKey('body')
+        ? widget.notice['body']
+        : Font.kEmptyString;
+    final String date = widget.notice.containsKey('date')
+        ? widget.notice['date']
+        : Font.kEmptyString;
+    final String access = widget.notice.containsKey('access')
+        ? widget.notice['access']
+        : Font.kEmptyString;
 
-    return Column(
-      children: [
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 20.0),
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                color: Theme.of(context).noticeBorderColor,
-                width: 1.0,
-              ),
-              top: BorderSide.none,
-              left: BorderSide.none,
-              right: BorderSide.none,
-            ),
-          ),
-        ),
-        InkWell(
-          splashColor: Colors.transparent,
-          highlightColor: Colors.transparent,
-          onTap: () async {
-            widget.markNoticeAsRead(widget.notice['id'].toString());
-            if (mounted) {
-              await navigateToWebPage(context);
-            }
-          },
-          child: ListTile(
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontFamily: Font.kDefaultFont,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.normal,
-                    color: textColor,
+    return (widget.notice.containsKey('id') && widget.notice['id'] == null)
+        ? const SizedBox.shrink()
+        : Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Theme.of(context).noticeBorderColor,
+                      width: 1.0,
+                    ),
+                    top: BorderSide.none,
+                    left: BorderSide.none,
+                    right: BorderSide.none,
                   ),
                 ),
-                const SizedBox(height: 4),
-                if (widget.notice.containsKey('body') &&
-                    widget.notice['body'] != null)
-                  Text(
-                    widget.notice['body'],
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontFamily: Font.kDefaultFont,
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.normal,
-                      color: textColor.withOpacity(0.8),
-                    ),
-                  ),
-              ],
-            ),
-            subtitle: Row(
-              children: [
-                Expanded(
-                  child: Row(
+              ),
+              InkWell(
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                onTap: () async {
+                  widget.markNoticeAsRead(widget.notice['id'].toString());
+                  if (mounted) {
+                    await navigateToWebPage(context);
+                  }
+                },
+                child: ListTile(
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      if (widget.notice['date'] != null)
-                        Text(
-                          date,
-                          style: TextStyle(
-                            fontFamily: Font.kDefaultFont,
-                            fontSize: 14.0,
-                            fontWeight: FontWeight.normal,
-                            color: textColor.withOpacity(0.6),
-                          ),
+                      Text(
+                        title,
+                        style: TextStyle(
+                          fontFamily: Font.kDefaultFont,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.normal,
+                          color: textColor,
                         ),
-                      const SizedBox(width: 5),
-                      if (widget.notice['access'] != null)
+                      ),
+                      const SizedBox(height: 4),
+                      if (body.isNotEmpty)
                         Text(
-                          '조회 $access',
+                          body,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
                           style: TextStyle(
                             fontFamily: Font.kDefaultFont,
                             fontSize: 14.0,
                             fontWeight: FontWeight.normal,
-                            color: textColor.withOpacity(0.6),
+                            color: textColor.withValues(alpha: (0.2 * 255)),
                           ),
                         ),
                     ],
                   ),
-                ),
-                // 북마크 아이콘 정의
-                IconButton(
-                  icon: Icon(
-                    widget.isBookmarked
-                        ? Icons.bookmark
-                        : Icons.bookmark_border,
-                    color: widget.isBookmarked ? Colors.yellow : Colors.grey,
+                  subtitle: Row(
+                    children: [
+                      Expanded(
+                        child: Row(
+                          children: [
+                            if (date.isNotEmpty)
+                              Text(
+                                date,
+                                style: TextStyle(
+                                  fontFamily: Font.kDefaultFont,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.normal,
+                                  color:
+                                      textColor.withValues(alpha: (0.2 * 255)),
+                                ),
+                              ),
+                            const SizedBox(width: 5),
+                            if (access.isNotEmpty)
+                              Text(
+                                '조회 $access',
+                                style: TextStyle(
+                                  fontFamily: Font.kDefaultFont,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.normal,
+                                  color:
+                                      textColor.withValues(alpha: (0.2 * 255)),
+                                ),
+                              ),
+                          ],
+                        ),
+                      ),
+                      // 북마크 아이콘 정의
+                      IconButton(
+                        icon: Icon(
+                          widget.isBookmarked
+                              ? Icons.bookmark
+                              : Icons.bookmark_border,
+                          color:
+                              widget.isBookmarked ? Colors.yellow : Colors.grey,
+                        ),
+                        onPressed: () async {
+                          if (widget.notice.containsKey('id') &&
+                              widget.notice['id'] != null) {
+                            await widget.toggleBookmark(widget.notice);
+                          }
+                        },
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                      ),
+                    ],
                   ),
-                  onPressed: () async {
-                    if (widget.notice.containsKey('id') &&
-                        widget.notice['id'] != null) {
-                      await widget.toggleBookmark(widget.notice);
-                    }
-                  },
-                  splashColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
                 ),
-              ],
-            ),
-          ),
-        )
-      ],
-    );
+              )
+            ],
+          );
   }
 }
