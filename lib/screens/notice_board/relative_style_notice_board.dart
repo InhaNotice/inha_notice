9a496e3a2842edf20bd04e1fddcad8f6d2ff6105
logicallaw <logicallawbio@gmail.com>
2025-02-10 15:13:57 +0900

    docs: update LICENSE

--- lib/screens/notice_board/relative_style_notice_board.dart
+++ lib/screens/notice_board/relative_style_notice_board.dart
@@ -1,3 +1,12 @@
+/*
+ * This is file of the project inha_notice
+ * Licensed under the Apache License 2.0.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * http://www.apache.org/licenses/
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-10
+ */
 import 'package:flutter/material.dart';
 import 'package:inha_notice/constants/page_constants.dart';
 import 'package:inha_notice/screens/notice_board/base_notice_board.dart';
--- lib/services/relative_style_scraper/base_relative_style_notice_scraper.dart
+++ lib/services/relative_style_scraper/base_relative_style_notice_scraper.dart
@@ -1,3 +1,12 @@
+/*
+ * This is file of the project inha_notice
+ * Licensed under the Apache License 2.0.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * http://www.apache.org/licenses/
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-10
+ */
 abstract class BaseRelativeStyleNoticeScraper {
   Future<Map<String, dynamic>> fetchNotices(int offset);
 
