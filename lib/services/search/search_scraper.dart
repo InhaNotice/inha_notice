1358ef72afa54c2ab78c5fb1f94c576e8970ea37
logicallaw <logicallawbio@gmail.com>
2025-02-10 15:13:57 +0900

    docs: update LICENSE

--- lib/services/search/search_scraper.dart
+++ lib/services/search/search_scraper.dart
@@ -1,3 +1,12 @@
+/*
+ * This is file of the project inha_notice
+ * Licensed under the Apache License 2.0.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * http://www.apache.org/licenses/
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-10
+ */
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:html/parser.dart' as html_parser;
 import 'package:http/http.dart' as http;
