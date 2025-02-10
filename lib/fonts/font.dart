3485dc00e1cb57f6812c8ed84a440b0b1e01e93c
logicallaw <logicallawbio@gmail.com>
2025-02-10 15:30:20 +0900

    docs(lib/font): update docs

--- lib/fonts/font.dart
+++ lib/fonts/font.dart
@@ -7,6 +7,9 @@
  * Author: junho Kim
  * Latest Updated Date: 2025-02-10
  */
+
+/// **Font**
+/// 이 클래스는 폰트와 관련된 상수를 정의하는 추상 클래스입니다.
 abstract class Font {
   static const String kDefaultFont = 'Pretendard';
   static const String kEmptyString = '';
