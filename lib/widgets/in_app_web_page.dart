7cf8b54031e56dd90d11bb9f0ee29b8322b5bbf9
logicallaw <logicallawbio@gmail.com>
2025-02-14 21:35:27 +0900

    build(android): 안드로이드 환경 웹 페이지 로딩 방식 변경

--- lib/widgets/in_app_web_page.dart
+++ lib/widgets/in_app_web_page.dart
@@ -7,7 +7,12 @@
  * Author: junho Kim
  * Latest Updated Date: 2025-02-10
  */
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:inha_notice/fonts/font.dart';
+import 'package:inha_notice/themes/theme.dart';
 import 'package:inha_notice/widgets/themed_snackbar.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -23,6 +28,8 @@ class InAppWebPage extends StatefulWidget {
 }
 
 class _InAppWebPageState extends State<InAppWebPage> {
+  InAppWebViewController? _webViewController;
+
   /// **url를 입력받아 웹 페이지를 로딩**
   Future<void> _launchInAppWebView(String url) async {
     final Uri uri = Uri.parse(url);
@@ -49,27 +56,106 @@ class _InAppWebPageState extends State<InAppWebPage> {
     }
   }
 
+  /// **현재 페이지 공유하기**
+  Future<void> _shareUrl(String url) async {
+    final Uri uri = Uri.parse(url);
+    await launchUrl(uri, mode: LaunchMode.externalApplication);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<void>(
-      future: _launchInAppWebView(widget.url),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        } else if (snapshot.hasError) {
-          return Scaffold(
-            appBar: AppBar(title: const Text('Error')),
-            body: Center(
-              child: Text('URL을 연결할 수 없습니다: ${snapshot.error}'),
+    /// iOS는 url_launcher 방식 사용
+    if (Platform.isIOS) {
+      return FutureBuilder<void>(
+        future: _launchInAppWebView(widget.url),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          } else if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(title: const Text('Error')),
+              body: Center(
+                child: Text('URL을 연결할 수 없습니다: ${snapshot.error}'),
+              ),
+            );
+          } else {
+            // URL이 열렸으면 빈 화면 반환
+            return const SizedBox.shrink();
+          }
+        },
+      );
+    } else {
+      /// Android는 flutter_inappwebview 사용
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+          centerTitle: true,
+          title: Text(
+            widget.url,
+            style: TextStyle(
+              fontFamily: Font.kDefaultFont,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: Theme.of(context).textTheme.bodyMedium?.color ??
+                  Theme.of(context).defaultColor,
             ),
-          );
-        } else {
-          // URL이 열렸으면 빈 화면 반환
-          return const SizedBox.shrink();
-        }
-      },
-    );
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.share_outlined),
+              onPressed: () async {
+                if (_webViewController != null) {
+                  WebUri? currentUrl = await _webViewController?.getUrl();
+                  if (currentUrl != null) {
+                    await _shareUrl(currentUrl.toString());
+                  } else {
+                    if (mounted) {
+                      ThemedSnackbar.showSnackbar(
+                          context, '현재 페이지 URL을 가져올 수 없습니다.');
+                    }
+                  }
+                }
+              },
+            ),
+          ],
+        ),
+        body: InAppWebView(
+          initialUrlRequest: URLRequest(url: WebUri(widget.url)),
+          initialSettings: InAppWebViewSettings(
+            javaScriptEnabled: true,
+            supportZoom: true, // 줌 활성화
+            useOnDownloadStart: true, // 다운로드 이벤트 활성화
+            allowsInlineMediaPlayback: true,
+          ),
+          onWebViewCreated: (controller) {
+            _webViewController = controller;
+          },
+          onDownloadStartRequest: (controller, request) async {
+            final Uri uri = Uri.parse(request.url.toString());
+            try {
+              if (await canLaunchUrl(uri)) {
+                await launchUrl(uri, mode: LaunchMode.externalApplication);
+              } else {
+                throw Exception();
+              }
+            } catch (e) {
+              if (mounted) {
+                ThemedSnackbar.showSnackbar(context, '파일 다운로드 실패');
+              }
+            }
+          },
+          onReceivedError: (controller, request, error) {
+            if (mounted) {
+              ThemedSnackbar.showSnackbar(
+                context,
+                '페이지 로딩 실패: ${error.description}',
+              );
+            }
+          },
+        ),
+      );
+    }
   }
 }
