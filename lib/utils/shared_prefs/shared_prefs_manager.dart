9a067cd9c50bb1b4ab669918610dfe4808eb751f
logicallaw <logicallawbio@gmail.com>
2025-02-26 23:44:58 +0900

    fix: '시스템' -> '시스템 설정'

--- lib/utils/shared_prefs/shared_prefs_manager.dart
+++ lib/utils/shared_prefs/shared_prefs_manager.dart
@@ -26,7 +26,7 @@ class SharedPrefsManager {
   // 캐싱 전략
   static final Map<String, dynamic> _cachedPrefs = {
     // 테마 모드 (기본값: system)
-    SharedPrefKeys.kUserThemeSetting: '시스템',
+    SharedPrefKeys.kUserThemeSetting: '시스템 설정',
     // 구독 리스트
     SharedPrefKeys.kSubscribedTopics: <String>{},
     // 앱 전체 공지
