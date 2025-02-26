3beb4b05f4646cc41f3b85ca0760683625c99034
logicallaw <logicallawbio@gmail.com>
2025-02-26 15:49:53 +0900

    feat: 기존 장학/채용 탭 URL를 인하대 공식 홈페이지에서 제공하는 URL로 변경
    - 탭 명칭도 (장학, 채용) -> (장학, 모집/채용)으로 변경

--- lib/screens/bottom_navigation/bottom_nav_bar_page.dart
+++ lib/screens/bottom_navigation/bottom_nav_bar_page.dart
@@ -5,7 +5,7 @@
  * For full license text, see the LICENSE file in the root directory or at
  * http://www.apache.org/licenses/
  * Author: junho Kim
- * Latest Updated Date: 2025-02-25
+ * Latest Updated Date: 2025-02-26
  */
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
