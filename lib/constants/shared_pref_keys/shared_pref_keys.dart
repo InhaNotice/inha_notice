30492c19ea79d5e1c69463305c6bc9a6c4fd338b
logicallaw <logicallawbio@gmail.com>
2025-02-26 16:32:53 +0900

    refactor: add shared_pref_keys.dart

---
+++ lib/constants/shared_pref_keys/shared_pref_keys.dart
@@ -0,0 +1,20 @@
+/*
+ * This is file of the project inha_notice
+ * Licensed under the Apache License 2.0.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * http://www.apache.org/licenses/
+ * Author: junho Kim
+ * Latest Updated Date: 2025-02-26
+ */
+
+abstract class SharedPrefKeys {
+  static const String subscribedTopics = 'subscribed_topics';
+  static const String isSubscribedToAllUsers = 'isSubscribedToAllUsers';
+  static const String previousMajorKey = 'previous-major-key';
+  static const String majorKey = 'major-key';
+  static const String academicNotification = 'academic-notification';
+  static const String majorNotification = 'major-notification';
+  static const String INTERNATIONAL = 'INTERNATIONAL';
+  static const String SWUNIV = 'SWUNIV';
+}
