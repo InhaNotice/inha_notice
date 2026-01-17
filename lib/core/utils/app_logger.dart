78930762aa9e00b3316b8ab89051cf25c2bfd38d
logicallaw <logicallawbio@gmail.com>
2026-01-17 16:30:47 +0900

    chore(core/utils): add AppLogger

---
+++ lib/core/utils/app_logger.dart
@@ -0,0 +1,33 @@
+/*
+ * This is file of the project inha_notice
+ * Licensed under the Apache License 2.0.
+ * Copyright (c) 2025 INGONG
+ * For full license text, see the LICENSE file in the root directory or at
+ * http://www.apache.org/licenses/
+ * Author: Junho Kim
+ * Latest Updated Date: 2026-01-17
+ */
+
+import 'package:logger/logger.dart';
+
+class AppLogger {
+  static final Logger _logger = Logger(
+    printer: PrettyPrinter(
+      methodCount: 1,
+      errorMethodCount: 5,
+      lineLength: 80,
+      colors: true,
+      printEmojis: true,
+    ),
+  );
+
+  static void d(String message) {
+    _logger.d(message);
+  }
+
+  static void e(String message, [Object? error, StackTrace? stackTrace]) {
+    _logger.e(message, error: error, stackTrace: stackTrace);
+
+    // TO-DO: FirebaseCrashlytics 기능 추가
+  }
+}
